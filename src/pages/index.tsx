790c7d05bcf0f6f201876598e3d8b7d1c280b77f
梅原 隆一 <r_umehara@MacBook-Pro.local>
2022-10-24 17:59:45 +0900

    fix: link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import { Box, HStack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import NextLink from 'next/link';
 import { CampaignCard } from 'src/components/organisms/CampaignCard';
 
 const Home: NextPage = () => (
   <Box width="100%">
     <p>とりあえずのキャンペーン部分</p>
     <HStack m={10}>
-      {Array.from({ length: 5 }, (v, k) => k).map(() => (
-        <CampaignCard campaignPath="/campaign" />
+      {Array.from({ length: 5 }, (v, k) => k).map((k) => (
+        <NextLink href="/campaign" passHref>
+          <a href="/campaign">
+            <CampaignCard key={k} />
+          </a>
+        </NextLink>
       ))}
     </HStack>
   </Box>
